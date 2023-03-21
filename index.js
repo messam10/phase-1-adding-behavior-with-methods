9947f396628b52f935df3e38c83a3a66252d93c3
messam10 <memaq1998.10.7@gmail.com>
2023-03-21 08:53:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+class Cat {
+    constructor(name, gender) {
+        this.name = name
+        this.gender = gender
+        this.speak = () => `${this.name} says meow!`;
+    }
+}
+
+class Dog {
+    constructor(name, gender) {
+        this.name = name
+        this.gender = gender
+        this.speak = () => `${this.name} says woof!`;
+    }
+}
+
+class Bird {
+    constructor(name, gender) {
+        this.name = name
+        this.gender = gender
+        this.speak = () => (this.gender === "male") ? `It's me! ${this.name}, the parrot!` : `${this.name} says squawk!`;
+    }
+}
+
+cat = new Cat("Sasha", "female");
+dog = new Dog("Rufio", "male");
+bird = new Bird("Pablo", "male");
+bird2 = new Bird("Mable", "female");
